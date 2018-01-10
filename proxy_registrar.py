0a211e3dfb808fa9ef55ab6a4ca4fbd4608b009a
javiergutierrez10 <32259222+javiergutierrez10@users.noreply.github.com>
2018-01-10 13:07:37 +0100

    Realizar envío RTP

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -11,14 +11,16 @@ import time
 import socketserver
 import random
 
+
 def WriteinFile(fichlog, mensaje):
-    #Escribe en el fichero de registro
+    # Escribe en el fichero de registro
     gmt = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time())) + ' '
     M = mensaje.split('\r\n')
     S = ' '.join(M)
     outfile = open(fichlog, 'a')
     outfile.write(gmt + S + '\n')
     outfile.close()
+
 
 class XML_PR(ContentHandler):
     "Clase de manejo XML"
@@ -28,14 +30,14 @@ class XML_PR(ContentHandler):
         self.misdatos = {}
 
     def startElement(self, name, attrs):
-		#Recoge los datos contenidos en las etiquetas
+        # Recoge los datos contenidos en las etiquetas
         dat_atrib = {}
         server = ['name', 'ip', 'puerto']
         database = ['path', 'passwdpath']
         log = ['path']
         etiquetas = {'server': server, 'database': database, 'log':
                      log}
-                     
+
         if name in etiquetas:
             for atributo in etiquetas[name]:
                 if attrs.get(atributo, "") != "":
@@ -43,9 +45,10 @@ class XML_PR(ContentHandler):
             self.misdatos[name] = dat_atrib
 
     def get_tags(self):
-        #Devuelve los datos del XML
+        # Devuelve los datos del XML
         return self.misdatos
-    
+
+
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
     """
@@ -63,22 +66,31 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         """
         line = self.rfile.read()
         mensaje = line.decode('utf-8')
-        expires = int(mensaje.split(' ')[3])
+        metodo = mensaje.split(' ')[0]
+        opcion = mensaje.split(' ')[3]
         ip_cliente = self.client_address[0]
         puerto_cliente = self.client_address[1]
-        print("El cliente con IP:" + str(ip_cliente) + " y Puerto:" + str(puerto_cliente) + " nos manda", mensaje)
+        print("El cliente con IP:" + str(ip_cliente) +
+              " y Puerto:" + str(puerto_cliente) + " nos manda:", mensaje)
         comprob = mensaje.split(" ")[-1]
-        if line.decode('utf-8').split(' ')[0] == "REGISTER":
-            name_cliente = line.decode('utf-8').split(' ')[1]
+        if metodo == "REGISTER":
+            expires = int(opcion)
+            name_cliente = line.decode('utf-8').split(':')[1]
+            name_cliente = name_cliente.split(':')[0]
             try:
-            	comprobar = int(comprob)
+                comprobar = int(comprob)
             except ValueError:
-            	Autorizacion = True
+                Autorizacion = True
             if len(self.clientes) == 0 and expires != 0 and not Autorizacion:
-                numero = str(random.randint(1,999999999999999999))
-                self.wfile.write(b"SIP/2.0 401 Unauthorized\r\nWWW Authenticate: Digest nonce=" + bytes('"' + numero + '"','utf-8'))
+                numero = str(random.randint(1, 999999999999999999))
+                self.wfile.write(b"SIP/2.0 401 Unauthorized\r\nWWW Authenticate: Digest nonce=" + bytes('"' + numero + '"', 'utf-8'))
+                WriteinFile(FicheroLog, "SIP/2.0 401 Unauthorized")
             elif len(self.clientes) == 0 and expires != 0 and Autorizacion:
-            	self.clientes[name_cliente] = ip_cliente
+                self.clientes[name_cliente] = ip_cliente
+                self.wfile.write(b"SIP/2.0 200 OK\r\n")
+            elif len(self.clientes) != 0 and expires != 0 and Autorizacion:
+                self.clientes[name_cliente] = ip_cliente
+                self.wfile.write(b"SIP/2.0 200 OK\r\n")
             elif len(self.clientes) != 0:
                 for nombre in self.clientes:
                     if name_cliente == nombre and expires != 0:
@@ -88,36 +100,62 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                         Borrar_Cliente = True
                         break
                     elif name_cliente != nombre and expires != 0:
-                        self.wfile.write(b"SIP/2.0 401 Unauthorized\r\n\r\n")  
+                        numero = str(random.randint(1, 999999999999999999))
+                        self.wfile.write(b"SIP/2.0 401 Unauthorized\r\nWWW Authenticate: Digest nonce=" + bytes('"' + numero + '"', 'utf-8'))
+                        WriteinFile(FicheroLog, "SIP/2.0 401 Unauthorized")
                         break
 
-        if Borrar_Cliente is True:
-            del self.clientes[name_cliente]
+            if Borrar_Cliente is True:
+                del self.clientes[name_cliente]
 
-        print(self.clientes)
-          
+            print(self.clientes)
+
+        elif metodo == "INVITE":
+            user = mensaje.split(":")[1]
+            user = user.split(" ")[0]
+            userinclients = False
+            for nombre in self.clientes:
+                if user == nombre:
+                    userinclients = True
+                    ipinvitado = self.clientes[user]
+                    ipanfitr = mensaje.split(" ")[5]
+                    puertoanfitr = mensaje.split(" ")[9]
+                    self.wfile.write(b"SIP/2.0 100 Trying\r\nSIP/2.0 180 Ringing\r\nSIP/2.0 200 OK\r\n")
+                    WriteinFile(FicheroLog, "SIP/2.0 100 Trying" +
+                                "SIP/2.0 180 Ringing SIP/2.0 200 OK")
+                    break
+            if not userinclients:
+                self.wfile.write(b"SIP/2.0 404 User Not Found\r\n")
+                WriteinFile(FicheroLog, "SIP/2.0 404 User Not Found")
+        elif metodo != "REGISTER" and metodo != "INVITE":
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n")
+            WriteinFile(FicheroLog, "SIP/2.0 405 Method Not Allowed")
+        else:
+            self.wfile.write(b"SIP/2.0 400 Bad Request\r\n")
+            WriteinFile(FicheroLog, "SIP/2.0 400 Bad Request")
+
+
 if __name__ == "__main__":
-   # Creamos servidor de eco y escuchamos
-	try:
-		parser = make_parser()
-		cHandler = XML_PR()
-		parser.setContentHandler(cHandler)
-		parser.parse(open(sys.argv[1]))
-		DatosPR_XML = cHandler.get_tags()
-		NAME_PR = DatosPR_XML['server']['name']
-		IP_PR = DatosPR_XML['server']['ip']
-		PUERTO_PR = int(DatosPR_XML['server']['puerto'])
-		FicheroLog = DatosPR_XML['log']['path']
-		WriteinFile(FicheroLog, "Listening...")
-		
-	except IndexError:
-		sys.exit("Usage: python3 uaserver.py config")
-	except FileNotFoundError:
-		sys.exit("El archivo: " + sys.argv[1] + " no existe")
-	serv = socketserver.UDPServer((IP_PR, PUERTO_PR), 		SIPRegisterHandler)
-	
-	print("Server " + NAME_PR + " listening at port " + str(PUERTO_PR))
-	try:      
-		serv.serve_forever()
-	except KeyboardInterrupt:
-		print("Finishing server proxy " + NAME_PR)
+    # Creamos servidor de eco y escuchamos
+    try:
+        parser = make_parser()
+        cHandler = XML_PR()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(sys.argv[1]))
+        DatosPR_XML = cHandler.get_tags()
+        NAME_PR = DatosPR_XML['server']['name']
+        IP_PR = DatosPR_XML['server']['ip']
+        PUERTO_PR = int(DatosPR_XML['server']['puerto'])
+        FicheroLog = DatosPR_XML['log']['path']
+        WriteinFile(FicheroLog, "Listening...")
+    except IndexError:
+        sys.exit("Usage: python3 uaserver.py config")
+    except FileNotFoundError:
+        sys.exit("El archivo: " + sys.argv[1] + " no existe")
+    serv = socketserver.UDPServer((IP_PR, PUERTO_PR), SIPRegisterHandler)
+
+    print("Server " + NAME_PR + " listening at port " + str(PUERTO_PR))
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finishing server proxy " + NAME_PR)
--- uaclient.py
+++ uaclient.py
@@ -12,21 +12,18 @@ import uaserver
 import time
 
 try:
-	
-	METHOD = sys.argv[2]
-	OPCION = sys.argv[3]
-	parser = make_parser()
-	cHandler = uaserver.XML_UA()
-	parser.setContentHandler(cHandler)
-	parser.parse(open(sys.argv[1]))
-	DatosUA_XML = cHandler.get_tags()
-	FicheroLog = DatosUA_XML['log']['path']
-	WriteinFile(FicheroLog, "Starting....")
-	
+    METHOD = sys.argv[2]
+    OPCION = sys.argv[3]
+    parser = make_parser()
+    cHandler = uaserver.XML_UA()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(sys.argv[1]))
+    DatosUA_XML = cHandler.get_tags()
+    FicheroLog = DatosUA_XML['log']['path']
+    WriteinFile(FicheroLog, "Starting....")
 except IndexError:
-    
     sys.exit("Usage: uaclient.py config metodo opcion")
-    
+
 SIP_CLIENT = DatosUA_XML['account']['username']
 PASSWORD_CLIENT = DatosUA_XML['account']['passwd']
 IP_CLIENT = DatosUA_XML['uaserver']['ip']
@@ -34,40 +31,57 @@ PUERTO_CLIENT = DatosUA_XML['uaserver']['puerto']
 IP_PROXY = DatosUA_XML['regproxy']['ip']
 PUERTO_PROXY = int(DatosUA_XML['regproxy']['puerto'])
 
-# Contenido que vamos a enviar
-#LINE = METHOD + " sip:" + SIPNAME_SERVER + "@" + IP_SERVER + " SIP/2.0\r\n"
-
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	my_socket.connect((IP_PROXY, PUERTO_PROXY))
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((IP_PROXY, PUERTO_PROXY))
 
-	print()
-	if METHOD == "REGISTER":
-		LINE = METHOD + " sip:" + SIP_CLIENT + ":" + PUERTO_CLIENT + 			" SIP/2.0\r\nExpires: " + OPCION
-	elif METHOD == "INVITE":
-		LINE = "ACK sip:" + SIPNAME_SERVER + "@" + IP_SERVER + " SIP/2.0\r\n"
-	
-	try:
-		WriteinFile(FicheroLog, "Sent to " + str(IP_PROXY) + ":" + str(PUERTO_PROXY) + ": " + LINE)
-		print("Enviando:\r\n" + LINE)
-		my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-	except ConnectionRefusedError:
-		WriteinFile(FicheroLog, "Error: No server listening at " + IP_PROXY + "port " + str(PUERTO_PROXY))
-		sys.exit("Error: No server listening")
-# Esperamos respuesta	
+    print()
+    if METHOD == "REGISTER":
+        LINE = METHOD + " sip:" + SIP_CLIENT + ":" + PUERTO_CLIENT
+        LINE = LINE + " SIP/2.0\r\nExpires: " + OPCION
+    elif METHOD == "INVITE":
+        LINE = METHOD + " sip:" + OPCION + " SIP/2.0\r\n"
+        LINE = LINE + "Content-Type: application/sdp\r\n"
+        V = "v=0 \r\n"
+        o = "o=" + SIP_CLIENT + " " + IP_CLIENT + " \r\n"
+        S = "s=misesion \r\n"
+        T = "t=0 \r\n"
+        M = "m=audio " + str(PUERTO_CLIENT) + " RTP \r\n"
+        LINE = LINE + V + o + S + T + M
+    else:
+        LINE = METHOD + " sip:" + SIP_CLIENT + ":" + PUERTO_CLIENT
+        LINE = LINE + " SIP/2.0\r\nExpires: " + OPCION
+    try:
+        WriteinFile(FicheroLog, "Sent to " + str(IP_PROXY) + ":"
+                    + str(PUERTO_PROXY) + ": " + LINE)
+        print("Enviando:\r\n" + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print("Recibido:")
+        mensajeresp = data.decode('utf-8')
+        print(mensajeresp)
+    except ConnectionRefusedError:
+        WriteinFile(FicheroLog, "Error: No server listening at " +
+                    IP_PROXY + "port " + str(PUERTO_PROXY))
+        sys.exit("\r\nError: No server listening")
+# Esperamos respuesta
 
-	data = my_socket.recv(1024)
-	print("Recibido:")
-	mensajeresp = data.decode('utf-8')
-	print(mensajeresp)
-	
-	if mensajeresp.split(" ")[1] == "401":
-		numero = mensajeresp.split('"')[1]
-		autorizacion = " \r\nAuthorization: Digest response=" + '"' + str(numero) + '"'
-		LINE = LINE + autorizacion
-		print("Enviando:\r\n" + LINE)
-		my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-		
-	print("Terminando...")
-	print("Fin.")
+    if METHOD == "REGISTER" and mensajeresp.split(" ")[1] == "401":
+        numero = mensajeresp.split('"')[1]
+        autorizacion = " \r\nAuthorization: Digest response="
+        autorizacion = autorizacion + '"' + str(numero) + '"'
+        LINE = LINE + autorizacion
+        print("Enviando:\r\n" + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print("Recibido:")
+        mensajeresp = data.decode('utf-8')
+        print(mensajeresp)
+    elif METHOD == "INVITE":
+        data = my_socket.recv(1024)
+        print("Recibido:")
+        mensajeresp = data.decode('utf-8')
+        print(mensajeresp)
+    print("Terminando...")
+    print("Fin.")
